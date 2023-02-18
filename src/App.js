d9d7689d21fbfe4574afc603fe387ef1a780a7ab
Sodiq Sanusi <ademilddiq@gmail.com>
2023-02-18 02:49:08 +0100

    Clearing up boilerplate code and setting up the web app
    Added a chosen font, created some basic variables to store the colors I'll be mostly using. Will be switching to a new branch now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log(process.env.REACT_APP_API_KEY, 'hii');
+
   return (
     <div className="App">
       <header className="App-header">

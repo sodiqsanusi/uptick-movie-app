5522c7a8935b39f43bf84c8e0504c51a4ae7c569
Sodiq Sanusi <ademilddiq@gmail.com>
2023-02-18 11:27:06 +0100

    Header styling (Mobile)
    Will start with the search bar component now

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
 import styles from './header.module.css';
+import TMDBAttributionImg from '../../images/tmdb_logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return ( 
     <header className={styles.container}>
-      This is the headerr
+      <Link to='/'>Uptick Movies</Link>
+      <div className={styles.imgContainer}><img src={TMDBAttributionImg} alt='This web application is powered by The Movie DB'/></div>
     </header>
   );
 }

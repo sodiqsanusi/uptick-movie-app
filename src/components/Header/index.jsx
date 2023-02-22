a28519ed028cd6645a6901ca9e9268cee3b6cd99
Sodiq Sanusi <ademilddiq@gmail.com>
2023-02-22 03:09:23 +0100

    Starting with responsiveness for Large-screen devices

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return ( 
     <header className={styles.container}>
-      <Link to='/'>Uptick Movies</Link>
-      <div className={styles.imgContainer}><img src={TMDBAttributionImg} alt='This web application is powered by The Movie DB'/></div>
+      <div className={styles.wrapper}>
+        <Link to='/'>Uptick Movies</Link>
+        <div className={styles.imgContainer}><img src={TMDBAttributionImg} alt='This web application is powered by The Movie DB'/></div>
+      </div>
     </header>
   );
 }
